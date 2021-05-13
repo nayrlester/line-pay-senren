2c914f9cc42087366234ac3929b8f42eba19ce33
Nayrlester <rbulot12@gmail.com>
2021-05-13 17:37:13 +0800

    Add echo bot

--- routes.js
+++ routes.js
@@ -1,4 +1,10 @@
 const linepay = require("./controller/linepay.controller");
+const line = require('@line/bot-sdk');
+
+const config = {
+    channelAccessToken: process.env.LINE_PAY_BOT_TOKEN,
+    channelSecret: process.env.LINE_PAY_BOT_SECRET,
+};
 
 module.exports = function(app,io){
 
@@ -11,4 +17,24 @@ module.exports = function(app,io){
     })
 
     app.get("/line-pay", linepay.pay_reserve);
+
+    app.post('/webhook', line.middleware(config), (req, res) => {
+        Promise
+          .all(req.body.events.map(handleEvent))
+          .then((result) => res.json(result))
+          .catch((err) => {
+            console.error(err);
+            res.status(500).end();
+        });
+    });
+}
+
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return Promise.resolve(null);
+    }
+  
+    const echo = { type: 'text', text: event.message.text };
+  
+    return client.replyMessage(event.replyToken, echo);
 }
