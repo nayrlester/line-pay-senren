9b377e6b983a0469485dc171b3a487685bd12d8c
Nayrlester <rbulot12@gmail.com>
2021-05-16 23:16:09 +0800

    added send liff url on terminal

--- routes.js
+++ routes.js
@@ -1,13 +1,16 @@
 const line = require('@line/bot-sdk');
+// const liff = require('@line/liff');
 const { v4: uuid } = require('uuid'); 
 const line_pay = require("line-pay");
 const cache = require("memory-cache");
+const { exec } = require("child_process");
+
 const myLiffId = process.env.LINE_PAY_LIFF_ID;
 
 const pay = new line_pay({
   channelId: process.env.LINE_PAY_CHANNEL_ID,
   channelSecret: process.env.LINE_PAY_CHANNEL_SECRET,
-  isSandbox: false
+  isSandbox: true
 });
 
 const config_bot = {
@@ -18,16 +21,12 @@ const config_bot = {
 module.exports = function(app,io){
 
     app.get('/', function(req, res) {
-        res.render('index.ejs')
+        res.render('liff_send_message.ejs')
     });
     
     app.get('/send-id', function(req, res) {
         res.json({id: myLiffId});
     });
-
-    app.get('/confirm', function(req, res){
-        res.render('confirm.ejs')
-    })
 
     app.get("/pay-reserve", (req, res) => {
         let options = {
@@ -37,12 +36,13 @@ module.exports = function(app,io){
             orderId: uuid(),
             confirmUrl: process.env.LINE_PAY_CONFIRM_URL,
         }
-    
+
         pay.reserve(options).then((response) => {
             let reservation = options;
             reservation.transactionId = response.info.transactionId;
             cache.put("transactionId", reservation.transactionId);
-            cache.put("paymentAccessToke", response.info.paymentAccessToken);
+            cache.put("paymentAccessToken", response.info.paymentAccessToken);
+            cache.put("orderId", reservation.orderId);
             res.redirect(response.info.paymentUrl.web);
         })
     })
@@ -66,27 +66,25 @@ module.exports = function(app,io){
 
         pay.confirm(optionsConfirm).then((response) => {
             if(response.returnMessage == 'Success.'){
-                console.log('Payment success')
+                console.log('Success')
+                exec(`liff init ${process.env.LINE_ACCESS_TOKEN}`);
+                exec(`liff send ${process.env.LINE_PAY_LIFF_ID} ${process.env.LINE_LIFF_USER_ID}`, (error, stdout, stderr) => {
+                    if (error) {
+                        console.log(`error: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        console.log(`stderr: ${stderr}`);
+                        return;
+                    }
+                    console.log(`stdout: ${stdout}`);
+                });
                 res.redirect('back');
             }else{
                 console.log('Payment Failed.')
             }
         })
     });
-
-    app.get(`/pay-capture`, (req, res) => {
-        let options = {
-            amount: 1,
-            currency: "JPY",
-            transactionId: req.query.transactionId
-        }
-
-        console.log(req.query.transactionId)
-    
-        pay.capture(options).then((response) => {
-            console,log(response.info.payInfo)
-        })
-    })
 
 }
 
