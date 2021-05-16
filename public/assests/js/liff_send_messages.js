ee3fc694173044a0e327452eb266c4c4439d325f
Nayrlester <rbulot12@gmail.com>
2021-05-16 13:09:52 +0800

    liff send messages v1

--- public/assests/js/liff_send_messages.js
+++ public/assests/js/liff_send_messages.js
@@ -1,32 +1,53 @@
 window.onload = function (e) {
-    // initialize and get basic information
-    // https://developers.line.me/en/reference/liff/#initialize-liff-app
+    // init で初期化。基本情報を取得。
+    // https://developers.line.me/ja/reference/liff/#initialize-liff-app
     liff.init(function (data) {
         getProfile();
         initializeApp(data);
     });
 
-    // Send message
+    // LIFF アプリを閉じる
+    // https://developers.line.me/ja/reference/liff/#liffclosewindow()
+    document.getElementById('closewindowbutton').addEventListener('click', function () {
+        liff.closeWindow();
+    });
+
+    // ウィンドウを開く
+    // https://developers.line.me/ja/reference/liff/#liffopenwindow()
+    document.getElementById('openwindowbutton').addEventListener('click', function () {
+        liff.openWindow({
+            url: 'https://line.me'
+        });
+    });
+
+    document.getElementById('openwindowexternalbutton').addEventListener('click', function () {
+        liff.openWindow({
+            url: 'https://line.me',
+            external: true
+        });
+    });
+
+    // メッセージの送信
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
-        // https://developers.line.me/en/reference/liff/#liffsendmessages()
+        // https://developers.line.me/ja/reference/liff/#liffsendmessages()
         liff.sendMessages([{
             type: 'text',
-            text: "Send text message"
+            text: "テキストメッセージの送信"
         }, {
             type: 'sticker',
             packageId: '2',
             stickerId: '144'
         }]).then(function () {
-            window.alert("Sent");
+            window.alert("送信完了");
         }).catch(function (error) {
             window.alert("Error sending message: " + error);
         });
     });
 };
 
-// Get profile and display
+// プロファイルの取得と表示
 function getProfile(){
-    // https://developers.line.me/en/reference/liff/#liffgetprofile()
+    // https://developers.line.me/ja/reference/liff/#liffgetprofile()
     liff.getProfile().then(function (profile) {
         document.getElementById('useridprofilefield').textContent = profile.userId;
         document.getElementById('displaynamefield').textContent = profile.displayName;
