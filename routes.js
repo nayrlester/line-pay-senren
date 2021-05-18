f667110b45d678b18055b418975769859eb3d511
Nayrlester <rbulot12@gmail.com>
2021-05-18 21:53:33 +0800

    add sending message to user

--- routes.js
+++ routes.js
@@ -68,7 +68,7 @@ module.exports = function(app,io){
         pay.confirm(optionsConfirm).then((response) => {
             if(response.returnMessage == 'Success.'){
                 let text_message = "Thank you from liff";
-                res.redirect(`https://line.me/R/share?text=${text_message}`)
+                res.redirect(`https://line.me/R/oaMessage/${myLiffId}/?${text_message}`)
                 // res.writeHead(200, {"Content-Type": "application/json"});
                 // const obj = {
                 //     success: true,
